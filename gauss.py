84e3db01877f1b6d2544c636d530bff704e05945
v1nte <ivinte99@gmail>
2020-09-11 18:12:50 -0300

    adding some comments, and trying to implement gaussian elimination

--- gauss.py
+++ gauss.py
@@ -16,12 +16,15 @@ def clear():
 #First, how many "Xs"
 shape = int(input("How many Xs?: "))
 
-arr = np.zeros((shape,1))
-arr2 = np.zeros((shape,shape))
-M = np.matrix(arr2)
-M = np.column_stack((M, arr))
+arr = np.zeros((shape,1)) #a zero vector
+arr2 = np.zeros((shape,shape))#a zero cuadratic array 
+M = np.matrix(arr2) #a zero matrix
+M = np.column_stack((M, arr))# append vector to Matrix
+
 for i in range(shape):
     for j in range(shape+1):
+        #fill the matrix
+        clear()
         print(M)
         num = int(input("Insert in [" +str(i+1) +str( j+1)+"]: "))
         M[i, j] = num 
@@ -30,4 +33,12 @@ for i in range(shape):
 print(M)
 
 
+for k in range(shape):
+    for i in range(k+1, shape+1):
+        for j in range(shape+1):
+            print(M)
+            M[i,j] = M[i,j] - ( (M[k,j] * M[i,j]) / M[k,k]  )
+        pass
+    pass
+print(M)
 
