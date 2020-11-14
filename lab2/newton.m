8d8558279e75e823279d4893aef4fe51656e3379
v1nte <ivinte99@gmail.com>
2020-11-14 19:36:30 -0300

    Refactor: try to be more modular

--- lab2/newton.m
+++ lab2/newton.m
@@ -1,14 +1,14 @@
-function x = newton(f,df,x0,tol,N) 
+function x = newton(f,df,x0,tol,N,ej) 
 
   f = @f;
   df = @df;
+
   x(1) = x0;
-  x(2) = x(1) - f(x(1))/df(f,x(1));
+  x(2) = x(1) - f(x(1), ej) / df(f,x(1), ej);
 
   i = 1;
-  
   while abs(x(i+1)-x(i)) > tol && i<=N
-    x(i+2) = x(i+1) -f(x(i+1))/df(f, x(i+1)); 
+    x(i+2) = x(i+1) -f(x(i+1), ej) / df(f, x(i+1), ej); 
     
     i = i + 1;
   end
