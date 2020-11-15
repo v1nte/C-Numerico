d18fbbcbfd67dd6549acd564065c66c4595ac3ba
v1nte <ivinte99@gmail.com>
2020-11-15 20:05:35 -0300

    Change function f(x) return

--- lab2/f.m
+++ lab2/f.m
@@ -1,18 +1,25 @@
 function y = f(x) 
+% Example functions
 %  y = x.^4-3*sqrt(x.^2-0.5)+1./x-10;
 %  y = x^x - 100;
 
-    h = 2.205;
-    l1 = 7.55;
-    l2 = 12.56;
 
-    y = ( h / abs( sqrt(l1^2-x^2) ) ) + ( h / abs( sqrt(l2^2 -x^2) ) ) - 1;
+%     Ejercicio 1
+%     A = 0.487;
+%     B = 2.198;
+%     C = 0.264;
+%     E = 1.189;
+%     y = A*sind(x)*cosd(x) + B*(sind(x)).^2 - C*cosd(x)-E*sind(x);
+%     Fin ejercicio 1
 
-    % Ejercicio 1
-    % A = 0.487;
-    % B = 2.198;
-    % C = 0.264;
-    % E = 1.189;
+%    Ejercicio 2
 
-    % y = A*sind(x)*cosd(x) + B*(sind(x)).^2 - C*cosd(x)-E*sind(x);
+%    Fin Ejercicio2
+
+%    Ejercicio 3
+%    h = 2.205;
+%    l1 = 7.55;
+%    l2 = 12.56;
+%    y = ( h / abs( sqrt(l1^2-x^2) ) ) + ( h / abs( sqrt(l2^2 -x^2) ) ) - 1;
+%    Fin Ejercicio 3
 end
