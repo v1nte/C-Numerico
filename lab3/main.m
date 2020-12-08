827042585004c4ab8c4fa8967e7a4f5b15e283e8
v1nte <ivinte99@gmail.com>
2020-12-08 06:09:58 -0300

    animation of angle1 on the entire trajectory

--- lab3/main.m
+++ lab3/main.m
@@ -1,7 +1,8 @@
 clc
 clear
+close all
 
-options = odeset('RelTol',1e-5,'AbsTol',[1e-5 1e-5 1e-5 1e-5]);
+options = odeset('RelTol', 1e-5,'AbsTol',[1e-5 1e-5 1e-5 1e-5]);
 
 % Estamos en la Tierra
 g = 9.81;
@@ -23,12 +24,36 @@ a = m2/m1;
 b = l2/l1;
 c = g/m1;
 
-a1 = pi/4;
+a1 = pi/2;
 v1 = 0;
-a2 = pi/6;
+a2 = pi/2;
 v2 = 0;
 
-t_max = 600;
+t_max = 20;
 
 
-[T,Y] = ode45(@(t,u)pendulo_doble(t, u, a,b,c),[0,t_max],[a1, v1, a2, v2], options);
+[T,U] = ode45(@(t,u)pendulo_doble(t, u, a,b,c),[0, t_max], [a1, v1, a2, v2], options);
+
+
+figure()
+for i=1:length(T)
+    %t_i = T(i);
+    %ang1_i = U(i,1)
+    %vel1_i = U(i,2)
+    %ang2_i = U(i,3)
+    %vel2_i = U(i,4)
+
+    
+    % Current
+    subplot(1, 1, 1);
+    plot(T(i), U(i,1), 'ro', 'LineWidth', 2, 'Markersize', 3);
+
+
+    % full circuit
+    hold on
+    plot(T, U(:,1), 'g-')
+    title(['Angulo en t = ', num2str(T(i)), ' segundos'])
+
+    drawnow
+end
+pause
--- lab3/pendulo_doble.m
+++ lab3/pendulo_doble.m
@@ -4,9 +4,9 @@ function du = pendulo_doble(t, u, a,b,c)
 
 
   du(1) = u(2);
-  du(2) = -( (1+a)*c*sind(u(1)) + a*b*u(3)^2*sind(u(1)-u(3)) + a*cosd(u(1)-u(3))*(u(1)^2*sind(u(1)-u(3))-c*sind(u(3))) ) / (1+a*sind(u(1)-u(3))^2);
+  du(2) = -( (1+a)*c*sin(u(1)) + a*b*u(3)^2*sin(u(1)-u(3)) + a*cos(u(1)-u(3))*(u(1)^2*sin(u(1)-u(3))-c*sin(u(3))) ) / (1+a*sin(u(1)-u(3))^2);
   du(3) = u(4);
-  du(4) = ( ((1+a)*(u(1)^2*sind(u(1)-u(3)))-c*sind(u(3)))+cosd(u(1)+u(3))*((1+a)*c*sind(u(1))+a*b*u(3)^2*sind(u(1)-u(3))) ) / (b*(1+a*sind(u(1)-u(2))^2));
+  du(4) = ( ((1+a)*(u(1)^2*sin(u(1)-u(3)))-c*sin(u(3)))+cos(u(1)+u(3))*((1+a)*c*sin(u(1))+a*b*u(3)^2*sin(u(1)-u(3))) ) / (b*(1+a*sin(u(1)-u(2))^2));
   
 
 end
