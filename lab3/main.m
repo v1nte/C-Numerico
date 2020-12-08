4a0ade09692aaf3843d2f91eb4bc11c1920da373
v1nte <ivinte99@gmail.com>
2020-12-08 07:46:20 -0300

    Fix orientation

--- lab3/main.m
+++ lab3/main.m
@@ -1,3 +1,4 @@
+clf
 clc
 clear
 close all
@@ -24,35 +25,54 @@ a = m2/m1;
 b = l2/l1;
 c = g/m1;
 
-a1 = pi/2;
+a1 = 1;
 v1 = 0;
-a2 = pi/2;
+a2 = 1;
 v2 = 0;
 
-t_max = 20;
+t_max = 25;
 
 
 [T,U] = ode45(@(t,u)pendulo_doble(t, u, a,b,c),[0, t_max], [a1, v1, a2, v2], options);
 
 
+% Rotate
+U(:,1) = U(:,1) - pi/2;
+U(:,3) = U(:,3) - pi/2;
+
 figure()
 for i=1:length(T)
+    
     %t_i = T(i);
     %ang1_i = U(i,1)
     %vel1_i = U(i,2)
     %ang2_i = U(i,3)
     %vel2_i = U(i,4)
 
-    
-    % Current
-    subplot(1, 1, 1);
-    plot(T(i), U(i,1), 'ro', 'LineWidth', 2, 'Markersize', 3);
+    pos_m1 = pol2cart(U(i,1), l1);
+    pos_m2 = pol2cart(U(i,3), l2);
+    pos_m2(1) = pos_m2(1) + pos_m1(1);
+    pos_m2(2) = pos_m2(2) + pos_m1(2);
 
+    % Current
+    subplot(2, 1, 1);
+    plot(T(i), U(i,1), 'ro', 'LineWidth', 2, 'Markersize', 3)
 
     % full circuit
+    plot(T, U(:,1), 'g-');
     hold on
-    plot(T, U(:,1), 'g-')
     title(['Angulo en t = ', num2str(T(i)), ' segundos'])
+
+    subplot(2,1,2)
+    plot(
+        [0 pos_m1(1)],
+        [0 pos_m1(2)]
+    ); 
+    hold on
+    plot(
+        [pos_m1(1) pos_m2(1)],
+        [pos_m1(2) pos_m2(2)]
+    );
 
     drawnow
 end
