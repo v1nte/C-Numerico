72884199406e45f8ab7153edabd0a7327e3efaa0
v1nte <ivinte99@gmail.com>
2020-12-14 19:35:56 -0300

    Add State-space formulation to coupled pendulum, could be wrong

--- lab3/pendulo_acoplado.m
+++ lab3/pendulo_acoplado.m
@@ -1,5 +1,11 @@
-function [T, Y] = pendulo_acoplado() 
+function [T, Y] = pendulo_acoplado(t, u, m1, m2, L, a, k, g) 
 
+  du = zeros(4,1);
+
+  du(1) = u(2);
+  du(2) = ( k*a*a* ( sin(u(3)) - sin(u(1)) ) * cos(u(1)) - cos(u(2)) - m1*L*g*sin(u(1)) ) / ( m1*L*L );
+  du(3) = u(4);
+  du(4) = -( k*a*a * ( sin(u(3)) - sin(u(1)) ) * cos(u(3)) - cos(u(4)) - m2*L*g*sin(u(3)) ) / ( m2*L*L );
 
 end
 
