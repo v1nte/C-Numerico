4eb580e46b58a2b1f53f1cba3a0ee0baf2942963
v1nte <ivinte99@gmail.com>
2020-12-15 01:37:23 -0300

    Fix typo in equation

--- lab3/pendulo_acoplado.m
+++ lab3/pendulo_acoplado.m
@@ -1,11 +1,10 @@
-function du = pendulo_acoplado(t, u, m1, m2, L, a, k, g) 
+function du = pendulo_acoplado(t, u, m1, m2, L, a, k, g, c) 
 
   du = zeros(4,1);
 
   du(1) = u(2);
-  du(2) = ( k*a*a* ( sin(u(3)) - sin(u(1)) ) * cos(u(1)) - cos(u(2)) - m1*L*g*sin(u(1)) ) / ( m1*L*L );
+  du(2) =  ( k*a^2 * ( sin(u(3)) - sin(u(1)) ) * cos(u(1)) - c*(u(2)) - m1*L*g*sin(u(1)) ) / ( m1*L^2 );
   du(3) = u(4);
-  du(4) = -( k*a*a * ( sin(u(3)) - sin(u(1)) ) * cos(u(3)) - cos(u(4)) - m2*L*g*sin(u(3)) ) / ( m2*L*L );
+  du(4) = -( k*a^2 * ( sin(u(3)) - sin(u(1)) ) * cos(u(3)) - c*(u(4)) - m2*L*g*sin(u(3)) ) / ( m2*L^2 );
 
 end
-
