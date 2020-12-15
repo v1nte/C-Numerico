e48c470c11aaa55935130137df1aae9afff664b7
v1nte <ivinte99@gmail.com>
2020-12-14 23:44:31 -0300

    Fix equation typo, i hope this is the last one

--- lab3/pendulo_doble.m
+++ lab3/pendulo_doble.m
@@ -3,9 +3,9 @@ function du = pendulo_doble(t, u, a,b,c)
   du = zeros(4,1);
 
   du(1) = u(2);
-  du(2) = -( (1+a)*c*sin(u(1)) + a*b*(u(4))^2*sin(u(1)-u(3)) + a*cos(u(1)-u(3))*(u(2)^2*sin(u(1)-u(3))-c*sin(u(3))) ) / (1+a*(sin(u(1)-u(3)))^2);
+  du(2) = -( (1+a)*c*sin(u(1)) + a*b*((u(4))^2)*sin( u(1)-u(3) ) + a*cos( u(1)-u(3) )*( (u(2))^2*sin( u(1)-u(3) )-c*sin(u(3)) ) ) / (1+a*( sin( u(1)-u(3) ) )^2);
   du(3) = u(4);
-  du(4) = ( ((1+a)*((u(2))^2*sin(u(1)-u(3)))-c*sin(u(3)))+cos(u(1)+u(3))*((1+a)*c*sin(u(1))+a*b*u(4)^2*sin(u(1)-u(3))) ) / (b*(1+a*(sin(u(1)-u(2)))^2));
+  du(4) = ( (1+a)*( (u(2))^2*sin( u(1)-u(3) ) - c*sin(u(3)) ) + cos( u(1)-u(3) ) * ( (1+a)*c*sin(u(1)) + a*b*(u(4))^2*sin( u(1)-u(3) )) ) / ( b*(1+a*(sin( u(1)-u(3) ))^2) );
 
   %theta1 = x(1);
   %theta2 = x(2);
